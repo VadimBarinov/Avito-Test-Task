84722cd34b1137e994d12e97afdbc77d9ec10b94
VadimBarinov <barinovvadim87@gmail.com>
2025-09-01 12:08:26 +0400

    Create auth utils

--- pvz_app/auth/utils.py
+++ pvz_app/auth/utils.py
@@ -1,3 +1,7 @@
+import uuid
+from datetime import timedelta, timezone, datetime
+
+import bcrypt
 import jwt
 
 from core.config import settings
@@ -6,6 +10,51 @@ auth_data = settings.auth_jwt
 
 
 def encode_jwt(
+        payload: dict,
+        private_key: str = auth_data.PRIVATE_KEY_PATH.read_text(),
+        algorithm: str = auth_data.ALGORITHM,
+        expire_timedelta: timedelta = 0,
+) -> str:
+    to_encode = payload.copy()
+    now = datetime.now(timezone.utc)
+    expires_in = now + expire_timedelta
+    to_encode.update(
+        exp=expires_in,
+        iat=now,
+        jti=str(uuid.uuid4())
+    )
+    return jwt.encode(
+        payload=to_encode,
+        key=private_key,
+        algorithm=algorithm,
+    )
 
-):
-    pass
+
+def decode_jwt(
+        token: str | bytes,
+        public_key: str = auth_data.PUBLIC_KEY_PATH.read_text(),
+        algorithm: str = auth_data.ALGORITHM,
+) -> dict:
+    return jwt.decode(
+        jwt=token,
+        key=public_key,
+        algorithms=algorithm,
+    )
+
+
+def hash_password(password: str) -> bytes:
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(
+        password=password.encode(),
+        salt=salt,
+    )
+
+
+def validate_password(
+        password: str,
+        hashed_password: bytes,
+) -> bool:
+    return bcrypt.checkpw(
+        password=password.encode(),
+        hashed_password=hashed_password,
+    )
