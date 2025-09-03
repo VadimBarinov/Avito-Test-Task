e3d742ca02a42e1612c3067456aa922258ad710f
VadimBarinov <barinovvadim87@gmail.com>
2025-09-03 19:37:42 +0400

    Refactor create_tables.sql

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -27,5 +27,6 @@ CREATE TABLE IF NOT EXISTS users (
     id UUID DEFAULT gen_random_uuid(),
     email VARCHAR(255) UNIQUE,
     password VARCHAR(255),
-
-)
+    is_employee BOOLEAN,
+    is_moderator BOOLEAN
+);
